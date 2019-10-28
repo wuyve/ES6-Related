3d426693df5ea16956a88e8a76abafdcfb72b1ca
wuyve <y80608991@126.com>
2019-10-28 10:20:53 +0800

    测试

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-
-
-
+let iterator01 = {
+    *[Symbol.iterator] () {
+        for(let i = 0; i < 10; i++) {
+            yield parseInt(10 * (i + Math.random()));
+        }
+    }
+}
+for(let n of iterator01) {
+    console.log(n);
+}
