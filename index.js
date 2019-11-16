9541e1722d35ea8e9ca467e66e162fc86b4d19b7
wuyve <y80608991@126.com>
2019-11-16 17:42:38 +0800

    更新完成：错误处理

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-class Person {
-    constructor(name = '?', age = '?', gender = '?', department = '?') {
-        this._name = name;
-        this._age = age;
-        this._gender = gender;
-        this._department = department;
-    }
-    set name(name) {
-        this._name = name;
-        console.log('Name is edited!');
-    }
-    get name() {
-        console.log('Name is got!');
-        return this._name;
-    }
-}
-let p01 = new Person();
-console.log(p01.name);
-p01.name = 'Japer';
-console.log(p01.name);
-console.log(p01._name);
+let radius = 10, height = 15, result = 0;
+result = Math.PI * (radius ** 2) * height;
+console.log(result);  // 4712.38898038469
+
+debugger;
+
+result = 2 * Math.PI * radius ** 2 + 2 * Math.PI * radius * height;
+console.log(result);  // 1570.7963267948967
+
+debugger;
+
+result = 4 / 3 * Math.PI * radius ** 3;
+console.log(result);  // 4188.790204786391
+
+debugger;
+
+result = 4 * Math.PI * radius ** 2;
+console.log(result);  // 1256.6370614359173
